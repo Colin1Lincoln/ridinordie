447c15b93d38beb378129225a4716bb51ab2176a
Colin1Lincoln <colin1lincoln@yandex.ru>
2023-03-08 16:16:05 +0300

    branch2_test

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NUM_PS=$(ps aux | wc -l );
+
+hg="Load is high"
+min="load is minimum"
+
+
+
+if [ $NUM_PS -gt 0 ]; then
+	echo "Load is found"
+
+	if [ $NUM_PS -lt 400 ]; then
+	echo $hg
+		else
+	echo $min
+	fi
+else
+	echo "Load in not found"
+fi
